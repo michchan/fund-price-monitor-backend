c7063978f73379c98097cbdc8adcb471b121857d
Michael Chan <michchandev@gmail.com>
2020-08-22 04:23:13 +0800

    Initial commit

---
+++ test/fund-price-monitor-backend.test.ts
@@ -0,0 +1,13 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as FundPriceMonitorBackend from '../lib/fund-price-monitor-backend-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new FundPriceMonitorBackend.FundPriceMonitorBackendStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT))
+});
