4d51fb8c31efa675928e7d1682efba18ec715de4
Michael Chan <michchandev@gmail.com>
2020-08-22 05:02:17 +0800

    Fix cdk package versioning issues

--- lib/fund-price-monitor-backend-stack.ts
+++ lib/fund-price-monitor-backend-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 
+
 export class FundPriceMonitorBackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
