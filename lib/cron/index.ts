5de273a98c9d9d7d7a5d7a3d934a09ba64685ada
Michael Chan <michchandev@gmail.com>
2020-08-25 21:21:24 +0800

    Remove unused

--- lib/cron/index.ts
+++ lib/cron/index.ts
@@ -1,8 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
-import * as events from '@aws-cdk/aws-events';
-import * as targets from '@aws-cdk/aws-events-targets';
-import * as path from 'path';
+
 
 
 function init (scope: cdk.Construct) {
