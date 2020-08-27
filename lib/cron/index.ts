db9d798265e02d8dafe3eeb6d3e2448aac37042f
Michael Chan <michchandev@gmail.com>
2020-08-27 21:14:15 +0800

    Rename

--- lib/cron/index.ts
+++ lib/cron/index.ts
@@ -5,21 +5,21 @@ import * as iam from '@aws-cdk/aws-iam';
 
 function init (scope: cdk.Construct) {
     // Create IAM roles for scraping handlers
-    const handlerRole = new iam.Role(scope, 'ScraperRole', {
+    const cronRole = new iam.Role(scope, 'CronRole', {
         assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com')
     });
     // Grant db access permissions for handler by assigning role
-    handlerRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonDynamoDBFullAccess'));
+    cronRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonDynamoDBFullAccess'));
     
-    // Handler for Manulife MPF performance scraping
-    const handler = new lambda.Function(scope, 'cron.index', {
+    // Handler scraping data and related cron job
+    const mainHandler = new lambda.Function(scope, 'cron.index', {
         code: lambda.Code.fromAsset('bundles/cron/handlers'),
         handler: 'index.handler',
         timeout: cdk.Duration.seconds(300),
         runtime: lambda.Runtime.NODEJS_12_X,
         memorySize: 610,
-        role: handlerRole
-    });    
+        role: cronRole,
+    });
 
     // Run every day at 6PM UTC
     // See https://docs.aws.amazon.com/lambda/latest/dg/tutorial-scheduled-events-schedule-expressions.html
