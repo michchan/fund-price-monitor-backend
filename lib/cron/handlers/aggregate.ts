676b128cf0d1ad3c007bb6b620dd5ffc5ea959ee
Michael Chan <michchandev@gmail.com>
2020-08-29 01:30:52 +0800

    Update aggregate function

--- lib/cron/handlers/aggregate.ts
+++ lib/cron/handlers/aggregate.ts
@@ -3,8 +3,14 @@ import { DynamoDBStreamHandler } from "aws-lambda";
 
 
 export const handler: DynamoDBStreamHandler = async (event, context, callback) => {
-    console.log({
-        event,
-        context,
+    event.Records.forEach((record) => {
+        console.log('Stream record: ', JSON.stringify(record, null, 2));
+
+        if (record.eventName === 'INSERT') {
+            // 1. Aggregation for latest price
+            // 2. Aggregation for price change rate per week
+            // 3. Aggregation for price change rate per month
+            // 4. Aggregation for price change rate per quarter
+        }
     })
 }
