ad8c9fb32e82cd71f78e112ffd60b2b2970f33ab
Michael Chan <michchandev@gmail.com>
2020-08-31 00:35:17 +0800

    Fix waitForStream

--- lib/AWS/dynamodb/waitForStream.ts
+++ lib/AWS/dynamodb/waitForStream.ts
@@ -9,13 +9,11 @@ type I = AWS.DynamoDBStreams.DescribeStreamInput
 type O = AWS.DynamoDBStreams.DescribeStreamOutput
 export type Result = O
 
-const waitForStream = (input: I): Promise<null | Result> => new Promise((resolve, reject) => {
-    return waitForService<I, O, AWS.AWSError>(
-        // Prevent `this` context problem
-        (...args) => dynamodbStreams.describeStream(...args), 
-        input, 
-        data => !!data?.StreamDescription
-    )
-})
+const waitForStream = (input: I): Promise<null | Result> => waitForService<I, O, AWS.AWSError>(
+    // Prevent `this` context problem
+    (...args) => dynamodbStreams.describeStream(...args), 
+    input, 
+    data => !!data?.StreamDescription
+)
 
 export default waitForStream
