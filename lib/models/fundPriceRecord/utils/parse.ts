9f680a2b1af8c249fe26c45e348655688889b7ca
Michael Chan <michchandev@gmail.com>
2020-09-02 02:57:07 +0800

    Add try catch

--- lib/models/fundPriceRecord/utils/parse.ts
+++ lib/models/fundPriceRecord/utils/parse.ts
@@ -18,34 +18,39 @@ export type Item = {
  * Parse a dynamodb item to FundPriceRecord
  */
 const parse = (attributeMap: DynamoDB.AttributeMap): FundPriceRecord => {
-    const {
-        [attr.COMPANY_CODE]: company_code,
-        [attr.TIME_SK]: timeSK,
-        [attr.COMPANY]: company,
-        [attr.NAME]: name,
-        [attr.PRICE]: price,
-        [attr.UPDATED_DATE]: updatedDate,
-        [attr.RISK_LEVEL]: riskLevel,
-        [attr.FUND_TYPE]: fundType,
-        [attr.INITIAL_PRICE]: initialPrice,
-        [attr.LAUNCHED_DATE]: launchedDate,
-    } = attributeMap as unknown as Item
-
-    return {
-        company: company.S as CompanyType,
-        // Get the last composite segment of `company_code`
-        code: company_code.S.split('_').pop() ?? '',
-        name: name.S,
-        updatedDate: updatedDate.S,
-        price: +price.N,
-        initialPrice: +initialPrice.N,
-        launchedDate: launchedDate.S,
-        riskLevel: riskLevel.S as RiskLevel,
-        // Get the last composite segment of `timeSK`
-        time: timeSK.S.split('_').pop() ?? '',
-        fundType: fundType.S as FundType,
-        // Get the first composite segment of `timeSK`
-        recordType: timeSK.S.split('_').shift() as RecordType,
+    try {
+        const {
+            [attr.COMPANY_CODE]: company_code,
+            [attr.TIME_SK]: timeSK,
+            [attr.COMPANY]: company,
+            [attr.NAME]: name,
+            [attr.PRICE]: price,
+            [attr.UPDATED_DATE]: updatedDate,
+            [attr.RISK_LEVEL]: riskLevel,
+            [attr.FUND_TYPE]: fundType,
+            [attr.INITIAL_PRICE]: initialPrice,
+            [attr.LAUNCHED_DATE]: launchedDate,
+        } = attributeMap as unknown as Item
+    
+        return {
+            company: company.S as CompanyType,
+            // Get the last composite segment of `company_code`
+            code: company_code.S.split('_').pop() ?? '',
+            name: name.S,
+            updatedDate: updatedDate.S,
+            price: +price.N,
+            initialPrice: +initialPrice.N,
+            launchedDate: launchedDate.S,
+            riskLevel: riskLevel.S as RiskLevel,
+            // Get the last composite segment of `timeSK`
+            time: timeSK.S.split('_').pop() ?? '',
+            fundType: fundType.S as FundType,
+            // Get the first composite segment of `timeSK`
+            recordType: timeSK.S.split('_').shift() as RecordType,
+        }   
+    } catch (error) {
+        console.log('ERROR ITEM: ', JSON.stringify(attributeMap, null, 2))
+        throw error
     }
 }
 
--- lib/models/fundPriceRecord/utils/parseChangeRate.ts
+++ lib/models/fundPriceRecord/utils/parseChangeRate.ts
@@ -19,33 +19,38 @@ export type Item = {
  * Parse a dynamodb item to FundPriceChangeRate
  */
 const parseChangeRate = (attributeMap: DynamoDB.AttributeMap): FundPriceChangeRate => {
-    const {
-        [attr.COMPANY_CODE]: company_code,
-        [attr.TIME_SK]: timeSK,
-        [attr.NAME]: name,
-        [attr.PRICE]: price,
-        [attr.PRICE_LIST]: priceList,
-        [attr.PRICE_CHANGE_RATE]: priceChangeRate,
-        [attr.PERIOD]: period,
-        [attr.UPDATED_DATE]: updatedDate,
-    } = attributeMap as unknown as Item
+    try {
+        const {
+            [attr.COMPANY_CODE]: company_code,
+            [attr.TIME_SK]: timeSK,
+            [attr.NAME]: name,
+            [attr.PRICE]: price,
+            [attr.PRICE_LIST]: priceList,
+            [attr.PRICE_CHANGE_RATE]: priceChangeRate,
+            [attr.PERIOD]: period,
+            [attr.UPDATED_DATE]: updatedDate,
+        } = attributeMap as unknown as Item
+        
+        const [company, code] = company_code.S.split('_') as [CompanyType, string]
 
-    const [company, code] = company_code.S.split('_') as [CompanyType, string]
-
-    return {
-        company,
-        // Get the last composite segment of `company_code`
-        code,
-        name: name.S,
-        price: +price.N,
-        priceChangeRate: +priceChangeRate.N,
-        priceList: priceList.NS,
-        // Get the last composite segment of `timeSK`
-        time: timeSK.S.split('@').pop() ?? '',
-        updatedDate: updatedDate.S,
-        period: period.S.split('_').pop() ?? '',
-        // Get the first composite segment of `timeSK`
-        recordType: timeSK.S.split('_').shift() as AggregatedRecordType,
+        return {
+            company,
+            // Get the last composite segment of `company_code`
+            code,
+            name: name.S,
+            price: +price.N,
+            priceChangeRate: +priceChangeRate.N,
+            priceList: priceList.NS,
+            // Get the last composite segment of `timeSK`
+            time: timeSK.S.split('@').pop() ?? '',
+            updatedDate: updatedDate.S,
+            period: period.S.split('_').pop() ?? '',
+            // Get the first composite segment of `timeSK`
+            recordType: timeSK.S.split('_').shift() as AggregatedRecordType,
+        }
+    } catch (error) {
+        console.log('ERROR ITEM: ', JSON.stringify(attributeMap, null, 2))
+        throw error
     }
 }
 
