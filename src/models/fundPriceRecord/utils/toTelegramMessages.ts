cbf4dfbe5e5e181bd87243c5bb64a472db243dcd
Michael Chan <michchandev@gmail.com>
2020-09-03 22:21:55 +0800

    Update message style

--- src/models/fundPriceRecord/utils/toTelegramMessages.ts
+++ src/models/fundPriceRecord/utils/toTelegramMessages.ts
@@ -18,22 +18,23 @@ const toTelegramMessages = (
     const { year, month, week, quarter } = getDateTimeDictionary(date);
     const dateOfMonth = zeroPadding(date.getDate(), 2);
 
+    // Derive title line
     const titleLine = `* ------ ${scheduleType.toUpperCase()} - ${company.toUpperCase()} - ${year}-${month}-${dateOfMonth} (week: ${week}, Q${quarter}) ------ *`
+    // Derive item lines
     const itemLines = items.map(({ code, name, price, priceChangeRate }, i) => {
         const order = `${i + 1}.`
-        const codeTag = `*${code}*`
-        const priceTag = `$${Number(price).toFixed(2)}`
+        const codeTag = `__${code}__`
+        const priceTag = `*$${Number(price).toFixed(2)}*`
         const priceRateTag = `(${Number(priceChangeRate) > 0 ? `+` : `-`}${Number(priceChangeRate).toFixed(2)}%)`
         
         switch (scheduleType) {
             case 'quarterly':
             case 'monthly':
             case 'weekly':
-                return [order, codeTag, name, priceTag, priceRateTag].join('  ');
+                return `${order}  ${codeTag}  -  ${name}  -  ${priceTag} ${priceRateTag}`
             case 'daily':
             default:
-                return [order, codeTag, name, priceTag].join('  ');
-        }
+                return `${order}  ${codeTag}  -  ${name}  -  ${priceTag}`        }
     });
 
     return parseLinesToChunks([titleLine, '', ...itemLines])
