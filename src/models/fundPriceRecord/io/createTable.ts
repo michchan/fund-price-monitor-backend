4298b7194b611fd784699ed0c5ccb175a158aa40
Michael Chan <michchandev@gmail.com>
2020-09-05 03:38:15 +0800

    Upgrade cdk to 1.62.0

--- src/models/fundPriceRecord/io/createTable.ts
+++ src/models/fundPriceRecord/io/createTable.ts
@@ -109,8 +109,8 @@ const getTableParams = (TableName: string): DynamoDB.CreateTableInput => ({
     // Every created table are regarded as a table containing the latest timeSK series data,
     // So assign the best capacity units.
     ProvisionedThroughput: {
-        ReadCapacityUnits: 1,
-        WriteCapacityUnits: 1,
+        ReadCapacityUnits: 3,
+        WriteCapacityUnits: 2,
     },
     GlobalSecondaryIndexes: [
         createInclusiveCSI({
