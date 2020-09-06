f0f44a557bbf1633f38c5fa52e62a66444648dc9
Michael Chan <michchandev@gmail.com>
2020-09-06 21:58:13 +0800

    Test selector

--- src/cron/scrapers/scrapeFromAIAMPF.ts
+++ src/cron/scrapers/scrapeFromAIAMPF.ts
@@ -41,7 +41,7 @@ export interface PriceDataRecord extends Pick<FundPriceRecord,
  */
 const getPricesDataFromHTML = async (page: puppeteer.Page): Promise<PriceDataRecord[]> => {
     // Wait for the elements we want
-    await page.waitForSelector('#fundpriceslist > table > tbody > tr:not(.header) > td');
+    await page.waitForSelector('#fundpriceslist > table > tbody > tr > td');
 
     // Query DOM data
     // * Constants/variables must be inside the scope of the callback function
