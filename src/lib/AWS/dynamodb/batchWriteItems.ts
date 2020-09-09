b082e8d33f2f60b63f4583f950ef76a978594d46
Michael Chan <michchandev@gmail.com>
2020-09-09 13:23:23 +0800

    Log length

--- src/lib/AWS/dynamodb/batchWriteItems.ts
+++ src/lib/AWS/dynamodb/batchWriteItems.ts
@@ -29,16 +29,18 @@ async function batchWriteItems <T, RT extends PT | DT> (
     // Send batch requests for each chunk
     return Promise.all(
         chunks.map((chunkedRecords, index): Promise<ChunkResult> => {
+            // Create items
+            const items = chunkedRecords.map(rec => ({
+                [mode === 'put' ? 'PutRequest' : 'DeleteRequest']: (
+                    serialize ? serialize(rec) : serialize
+                ) as unknown as RT,
+            }))
             // Create request items
             const RequestItems: DynamoDB.DocumentClient.BatchWriteItemInput['RequestItems'] = {
-                [tableName]: chunkedRecords.map(rec => ({
-                    [mode === 'put' ? 'PutRequest' : 'DeleteRequest']: (
-                        serialize ? serialize(rec) : serialize
-                    ) as unknown as RT,
-                }))
+                [tableName]: items
             }
             // Log to console 
-            console.log(`Batch Write Request Items (chunk: ${index}): `, JSON.stringify(RequestItems, null, 2))
+            console.log(`Batch Write Request Items (chunk: ${index}, length: ${items}): `, JSON.stringify(RequestItems, null, 2))
 
             // Send batch create requests
             return docClient.batchWrite({ RequestItems }).promise()
