d7bad7d3bd229dd22cd0adfcb2c8586d9f3f9b64
Michael Chan <michchandev@gmail.com>
2020-09-18 15:51:17 +0800

    Add API gateway

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,8 @@
 import * as cdk from '@aws-cdk/core';
 import * as iam from '@aws-cdk/aws-iam';
 import { Effect } from '@aws-cdk/aws-iam';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigateway from '@aws-cdk/aws-apigateway';
 
 
 function init (scope: cdk.Construct) {
@@ -32,6 +34,51 @@ function init (scope: cdk.Construct) {
     }));
 
     /** ------------------ Lambda Handlers Definition ------------------ */
+
+    // Common input for lambda Definition
+    const commonLambdaInput = {
+        code: lambda.Code.fromAsset('bundles/cron/handlers'),
+        timeout: cdk.Duration.minutes(5),
+        runtime: lambda.Runtime.NODEJS_12_X,
+        memorySize: 250,
+    };
+
+    /**
+     * Handler for getting list of records of a single fund
+     */
+    const getSingleFundRecordsHandler = new lambda.Function(scope, 'GetSingleFundRecords', {
+        ...commonLambdaInput,
+        handler: 'records.single.list.handler',
+    });
+    /**
+     * Handler for getting list of records
+     */
+    const getComRecordsHandler = new lambda.Function(scope, 'GetCompanyRecords', {
+        ...commonLambdaInput,
+        handler: 'records.list.handler',
+    });
+    /**
+     * Handler for getting list of rates
+     */
+    const getComRatesHandler = new lambda.Function(scope, 'GetCompanyRates', {
+        ...commonLambdaInput,
+        handler: 'rates.list.handler',
+    });
+
+    /** ------------------ API Gateway Definition ------------------ */
+
+    // Create api service
+    const api = new apigateway.RestApi(scope, 'MPFFundPricesApi', {
+        restApiName: 'MPF Fund Prices Service',
+    });
+
+    // Add records path
+    const allRecords = api.root.addResource('mpf/funds');
+    const comRecords = allRecords.addResource('{company}');
+    const singleFundRecords = comRecords.addResource('{code}');
+    const weekRates = comRecords.addResource('week/{week}');
+    const monthRates = comRecords.addResource('month/{month}');
+    const quarterRates = comRecords.addResource('quarter/{quarter}');
 
     
 }
