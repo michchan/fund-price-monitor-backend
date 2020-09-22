f4040bd9aeddd7e76756ea901f7d6f10f28687cf
Michael Chan <michchandev@gmail.com>
2020-09-22 11:40:56 +0800

    Create StructuredQuery

--- src/api/handlers/searchRecords.ts
+++ src/api/handlers/searchRecords.ts
@@ -4,6 +4,7 @@ import { DocumentClient } from "aws-sdk/clients/dynamodb";
 import { ListResponse } from "../Responses.type";
 import { FundPriceRecord } from '../../models/fundPriceRecord/FundPriceRecord.type'
 import createReadResponse from "../helpers/createReadResponse";
+import { StructuredQueryString } from "../StructuredQuery.type";
 
 
 export type Res = ListResponse<FundPriceRecord>;
@@ -11,35 +12,7 @@ export type Res = ListResponse<FundPriceRecord>;
 export interface QueryParams {
     latest?: boolean;
     exclusiveStartKey?: DocumentClient.QueryInput['ExclusiveStartKey'];
-    /**
-     * Structured query string:
-     *
-     * 1. Format: FIELD_NAME_1[OPERATOR]VALUE_1+FIELD_NAME_N[OPERATOR]VALUE_N
-     * e.g. company[e]aia+code[e]123+name[inc]healthcare+updatedDate[gte]2020-09-10
-     *
-     * 2. Operators: 
-     * 
-     *   For general type:
-     *    - `e` : is equal to.
-     *    - `ie` : is inequal to.
-     *    - `gt` : is greater than.
-     *    - `lt` : is lower than.
-     *    - `gte` : is greater than or equal to.
-     *    - `lte` : is greater than or equal to.
-     *    - `between` : is between A and B. 
-     *      e.g. To query records of updated date in between 2020-09-10 (lower bound) and 2020-09-20 (upper bound),
-     *      the expression will be: updatedDate[between]2020-09-10,2020-09-20
-     * 
-     *   For string only
-     *    - `inc` : includes the word(s)
-     *      e.g. name[inc]healthcare,growth,fund
-     *    - `notinc` : does not include the word(s)
-     *    - `beginswith` : begins with word(s)
-     * 
-     * 3. List values: separated by commas
-     * e.g. name[inc]healthcare,growth,fund
-     */
-    q?: string;
+    q?: StructuredQueryString;
 }
 
 /** 
---
+++ src/api/helpers/parseQuery.ts
@@ -0,0 +1,9 @@
+import { StructuredQuery, StructuredQueryString } from "../StructuredQuery.type"
+
+
+
+const parseQuery = (q: StructuredQueryString): StructuredQuery => {
+    return {}
+}
+
+export default parseQuery
