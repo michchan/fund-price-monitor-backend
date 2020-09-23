a214d10cbf4040e0f7127b3ff7a263987ac221a7
Michael Chan <michchandev@gmail.com>
2020-09-23 16:19:48 +0800

    Separate index and entry files

--- src/lib/AWS/index.ts
+++ src/lib/AWS/index.ts
@@ -1,9 +1,1 @@
-/**
- * Script to configure and export AWS sdk instance
- */
-import AWS = require('aws-sdk');
-
-// Update configs
-AWS.config.update({ region: 'ap-east-1' })
-
-export default AWS
+export { default } from './AWS'
