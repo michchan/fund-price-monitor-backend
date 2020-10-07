160fa4230b151f50c1dbe6a18a935fa217dc02ee
Michael Chan <michchandev@gmail.com>
2020-10-07 21:25:24 +0800

    Update retryWithDelay

--- src/cron/helpers/retryWithDelay.ts
+++ src/cron/helpers/retryWithDelay.ts
@@ -1,14 +1,17 @@
 import retry from 'simply-utils/dist/async/retry'
 
 
-const DEFAULT_RETRY_TIME = 5
-const DEFAULT_DELAY = 300
+const DEFAULT_RETRY_TIME = 10
+const DEFAULT_DELAY = 500
 
 function retryWithDelay <T = unknown> (
     callback: () => Promise<T>,
     logScope: string,
 ): Promise<T | void> {
     return retry<T>(callback, DEFAULT_RETRY_TIME, DEFAULT_DELAY)
-        .catch(err => console.log(`ERROR at retryWithDelay (${logScope}): `, JSON.stringify(err, null, 2)));
+        .catch(err => {
+            console.log(`ERROR at retryWithDelay (${logScope}): `, JSON.stringify(err, null, 2))
+            throw err
+        });
 }
 export default retryWithDelay
