10d9245f3cf769e1a6df4d49cddf387ab6a14042
Michael Chan <michchandev@gmail.com>
2020-10-11 23:28:37 +0800

    Create and init logging service

--- src/fund-price-monitor-backend-stack.ts
+++ src/fund-price-monitor-backend-stack.ts
@@ -1,15 +1,18 @@
 import * as cdk from '@aws-cdk/core';
 import cron from './services/cron';
 import api from './services/api';
+import logging from './services/logging';
 
 
 export class FundPriceMonitorBackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Initialize cron jobs
+    // Initialize cron service
     cron.init(this);
-    // Initilaize API handling
+    // Initialize API service
     api.init(this);
+    // Initialize logging service
+    logging.init(this);
   }
 }
---
+++ src/services/logging/logging.ts
@@ -0,0 +1,9 @@
+import * as cdk from '@aws-cdk/core';
+
+
+function init (scope: cdk.Construct) {
+    
+}
+
+const logging = { init } as const
+export default logging
