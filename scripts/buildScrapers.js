1dc56a6cf5eeeabb34bdd261de67fb8335d8727d
Michael Chan <michchandev@gmail.com>
2020-10-12 13:54:56 +0800

    Update build scrapers

--- scripts/buildScrapers.js
+++ scripts/buildScrapers.js
@@ -1,13 +1,30 @@
 const fs = require('fs')
 
 
-const buildScrapers = (isTest = false) => {
-    console.log(__dirname)
-    // const scrapers = fs.readdirSync()
+const rootDir = __dirname.replace(/\/scripts/i, '')
+const scrapersDir = `src/services/cron/scrapers`
+const scrapersDirAbs = `${rootDir}/${scrapersDir}`
+const templateDir = `src/services/cron/templates`
+const templateDirAbs = `${rootDir}/${templateDir}`
+
+const removePathExtension = (path) => path.replace(/\.(ts|js)/i, '')
+
+const buildScrapers = () => {
+    // Get names of scrapers
+    const scrapers = fs.readdirSync(scrapersDirAbs)
+        .map(path => removePathExtension(path))
+
+    // Get import path of scrapers
+    const scraperPaths = scrapers.map(path => `${scrapersDir}/${path}`)
+
+    console.log({ scrapers, scraperPaths })
+
+    // Read template paths
+    const templatePaths = fs.readdirSync(templateDirAbs)
+        .map(path => `${templateDirAbs}/${removePathExtension(path)}`)
+
+    console.log({ templatePaths })
 }
 
 // Run script
-// Build scrapers
 buildScrapers()
-// Build scraper testers
-buildScrapers(true)
