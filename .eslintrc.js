64f0c8187e73f1ccfc36ef8b991851c7e932879d
Michael Chan <michchandev@gmail.com>
2020-10-24 18:45:18 +0800

    Add   "ignoreRegExpLiterals": true, to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -136,6 +136,7 @@ module.exports = {
       "tabWidth": 2,
       "ignoreUrls": true,
       "ignoreStrings": true,
+      "ignoreRegExpLiterals": true,
       // Ignore require and single-line imports from very long module
       "ignorePattern":
         // Ignore require statement
