8f6dbfdf85a3447f00a6f4ccb2ff38a084ef87d7
Michael Chan <michchandev@gmail.com>
2020-10-29 13:31:25 +0800

    Add request delay

--- src/services/cron/helpers/notifyByTelegram.ts
+++ src/services/cron/helpers/notifyByTelegram.ts
@@ -1,8 +1,11 @@
 import pipeAsync from 'simply-utils/dist/async/pipeAsync'
+import wait from 'simply-utils/dist/async/wait'
 
 import notifyCompanyRecordsByTelegram, { ScheduleType } from './notifyCompanyRecordsByTelegram'
 import getTelegramApiCredentials from 'src/helpers/getTelegramApiCredentials'
 import getTableDetails from 'src/models/fundPriceRecord/io/getTableDetails'
+
+const REQUEST_DELAY = 500
 
 const notifyByTelegram = async (scheduleType: ScheduleType): Promise<void> => {
   /** -------- Get credentials for sending notifications -------- */
@@ -14,7 +17,10 @@ const notifyByTelegram = async (scheduleType: ScheduleType): Promise<void> => {
 
   /** -------- Notify for each company -------- */
   await pipeAsync(...companies.map(
-    company => () => notifyCompanyRecordsByTelegram(chatId, apiKey, company, scheduleType)
+    (company, i, arr) => async () => {
+      await notifyCompanyRecordsByTelegram(chatId, apiKey, company, scheduleType)
+      if (i < arr.length) await wait(REQUEST_DELAY)
+    }
   ))()
 }
 
