7e4709fe76fd829c9255cab597523275fcc8d997
Michael Chan <michchandev@gmail.com>
2020-11-01 00:57:32 +0800

    Use pipeByCompany

--- src/services/migration/handlers/dynamodbToS3.ts
+++ src/services/migration/handlers/dynamodbToS3.ts
@@ -1,4 +1,5 @@
 import { Handler } from 'aws-lambda'
+import { AttributeMap } from 'aws-sdk/clients/dynamodb'
 import pipeAsync from 'simply-utils/dist/async/pipeAsync'
 import wait from 'simply-utils/dist/async/wait'
 import omit from 'lodash/omit'
@@ -6,32 +7,41 @@ import omit from 'lodash/omit'
 import listTables from 'src/models/fundPriceRecord/io/listTables'
 import fromTableName from 'src/models/fundPriceRecord/utils/fromTableName'
 import queryItemsByCompany, { EXP_TIME_SK } from 'src/models/fundPriceRecord/io/queryItemsByCompany'
-import forEachCompany from 'src/models/fundPriceRecord/utils/forEachCompany'
+// !import AWS from 'src/lib/AWS'
+import { CompanyType } from 'src/models/fundPriceRecord/FundPriceRecord.type'
+import pipeByCompany from 'src/models/fundPriceRecord/utils/pipeByCompany'
 
+// != new AWS.S3()
 const TABLE_BATCH_DELAY = 1000
+
+const getCompanyManipulator = (tableName: string) => async (company: CompanyType) => {
+  const output = await queryItemsByCompany(company, {
+    shouldQueryAll: true,
+    at: fromTableName(tableName),
+    input: defaultInput => ({
+      // Cancel filter expression
+      FilterExpression: undefined,
+      // Remove unused values
+      ExpressionAttributeValues: omit(
+        defaultInput.ExpressionAttributeValues,
+        EXP_TIME_SK
+      ),
+    }),
+  })
+  console.log(JSON.stringify({ company, length: output.Items?.length }))
+  return output.Items ?? []
+}
 
 export const handler: Handler = async () => {
   // Get list of table names
   const tableNames = await listTables()
   // Manipulation for each table
   await pipeAsync(...tableNames.map((tableName, i, arr) => async () => {
-    await forEachCompany(async company => {
-      const output = await queryItemsByCompany(company, {
-        shouldQueryAll: true,
-        shouldQueryLatest: false,
-        at: fromTableName(tableName),
-        input: defaultInput => ({
-          // Cancel filter expression
-          FilterExpression: undefined,
-          // Remove unused values
-          ExpressionAttributeValues: omit(
-            defaultInput.ExpressionAttributeValues,
-            EXP_TIME_SK
-          ),
-        }),
-      })
-      console.log(JSON.stringify({ i, company, length: output.Items?.length }))
-    })
+    const records = await pipeByCompany<AttributeMap[]>(input => async company => {
+      const companyRecords = await getCompanyManipulator(tableName)(company)
+      return [...input, ...companyRecords]
+    }, [])
+    console.log(JSON.stringify({ i, tableName, length: records.length }, null, 2))
     if (i < arr.length - 1) await wait(TABLE_BATCH_DELAY)
   }))()
 }
