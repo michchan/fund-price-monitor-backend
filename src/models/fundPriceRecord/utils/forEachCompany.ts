4d3e54381db51eb6b1be8b6a73411d357d95e2f9
Michael Chan <michchandev@gmail.com>
2020-10-31 23:17:38 +0800

    Update comment

--- src/models/fundPriceRecord/utils/forEachCompany.ts
+++ src/models/fundPriceRecord/utils/forEachCompany.ts
@@ -4,18 +4,16 @@ import { CompanyType } from 'src/models/fundPriceRecord/FundPriceRecord.type'
 
 import getTableDetails from 'src/models/fundPriceRecord/io/getTableDetails'
 
-const DEFAULT_REQUEST_DELAY = 1000
+const DEFAULT_DELAY = 1000
 export type Iteratee <T> = (company: CompanyType, i: number, arr: CompanyType[]) => Promise<T>
 
 const forEachCompany = async <T = unknown> (
   iteratee: Iteratee<T>,
-  delay: number = DEFAULT_REQUEST_DELAY
+  delay: number = DEFAULT_DELAY
 ): Promise<void> => {
-  /** -------- Get list of companies -------- */
-  // Get from table-level "details" record
+  // Get list of companies from table-level "details" record
   const { companies } = await getTableDetails()
-
-  /** -------- Process for each company -------- */
+  // Manipulation for each company
   await pipeAsync(...companies.map(
     (company, i, arr) => async () => {
       await iteratee(company, i, arr)
