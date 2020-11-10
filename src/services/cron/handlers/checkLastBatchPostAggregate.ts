015992b02236a34eebad1109ce02529fe1560fb2
Michael Chan <michchandev@gmail.com>
2020-11-11 00:57:59 +0800

    Return false when there is no comScrapeMeta

--- src/services/cron/handlers/checkLastBatchPostAggregate.ts
+++ src/services/cron/handlers/checkLastBatchPostAggregate.ts
@@ -1,4 +1,5 @@
 import { Handler } from 'aws-lambda'
+import logObj from 'src/helpers/logObj'
 import getTableDetails from 'src/models/fundPriceRecord/io/getTableDetails'
 import queryItemsByCompany from 'src/models/fundPriceRecord/io/queryItemsByCompany'
 import parse from 'src/models/fundPriceRecord/utils/parse'
@@ -6,6 +7,7 @@ import parse from 'src/models/fundPriceRecord/utils/parse'
 export const handler: Handler<any, boolean> = async () => {
   const areAllBatchesAggregated = await (async () => {
     const { companies, scrapeMeta } = await getTableDetails()
+
     for (const company of companies) {
       const output = await queryItemsByCompany(company, {
         shouldQueryLatest: true,
@@ -13,7 +15,10 @@ export const handler: Handler<any, boolean> = async () => {
       })
       const latestItems = (output.Items ?? []).map(parse)
       const comScrapeMeta = scrapeMeta[company]
-      if (!comScrapeMeta) continue
+      if (!comScrapeMeta) {
+        logObj(`Some scrape metadata are not ready for company: ${company}`, scrapeMeta)
+        return false
+      }
 
       // The latest aggregated items are supposed to be create later than the comScrapeMeta.time
       const numAggregated = latestItems.filter(item => (
