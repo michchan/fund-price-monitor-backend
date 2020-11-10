33fb0d8abab9fdfe559484f476aab75b71fe9ea9
Michael Chan <michchandev@gmail.com>
2020-11-10 20:33:49 +0800

    Log isLastBatch

--- src/services/cron/handlers/checkLastBatchPostAggregate.ts
+++ src/services/cron/handlers/checkLastBatchPostAggregate.ts
@@ -4,24 +4,28 @@ import queryItemsByCompany from 'src/models/fundPriceRecord/io/queryItemsByCompa
 import parse from 'src/models/fundPriceRecord/utils/parse'
 
 export const handler: Handler<any, boolean> = async () => {
-  const { companies, scrapeMeta } = await getTableDetails()
-  for (const company of companies) {
-    const output = await queryItemsByCompany(company, {
-      shouldQueryLatest: true,
-      shouldQueryAll: true,
-    })
-    const latestItems = (output.Items ?? []).map(parse)
-    const comScrapeMeta = scrapeMeta[company]
-    if (!comScrapeMeta) continue
+  const isLastBatch = await (async () => {
+    const { companies, scrapeMeta } = await getTableDetails()
+    for (const company of companies) {
+      const output = await queryItemsByCompany(company, {
+        shouldQueryLatest: true,
+        shouldQueryAll: true,
+      })
+      const latestItems = (output.Items ?? []).map(parse)
+      const comScrapeMeta = scrapeMeta[company]
+      if (!comScrapeMeta) continue
 
-    // The latest aggregated items are supposed to be create later than the comScrapeMeta.time
-    const numAggregated = latestItems.filter(item => (
-      new Date(item.time).getTime() >= new Date(comScrapeMeta.time).getTime()
-    )).length
-    // * Return false to indicate that there are some items still being processed
-    if (numAggregated <= comScrapeMeta.size) return false
-  }
-  // * Return true to indicate that
-  // All batches of companies of records have been aggregated successfully after scraped
-  return true
+      // The latest aggregated items are supposed to be create later than the comScrapeMeta.time
+      const numAggregated = latestItems.filter(item => (
+        new Date(item.time).getTime() >= new Date(comScrapeMeta.time).getTime()
+      )).length
+      // * Return false to indicate that there are some items still being processed
+      if (numAggregated <= comScrapeMeta.size) return false
+    }
+    // * Return true to indicate that
+    // All batches of companies of records have been aggregated successfully after scraped
+    return true
+  })()
+  console.log('isLastBatch:', isLastBatch)
+  return isLastBatch
 }
