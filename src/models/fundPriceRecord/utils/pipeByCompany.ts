30f538a939a7994cab186d70af4c9ee9c5cc3f36
Michael Chan <michchandev@gmail.com>
2020-11-11 11:22:10 +0800

    Pass table details for pipeByCompany

--- src/models/fundPriceRecord/utils/pipeByCompany.ts
+++ src/models/fundPriceRecord/utils/pipeByCompany.ts
@@ -1,11 +1,16 @@
 import pipeAsync from 'simply-utils/dist/async/pipeAsync'
 import wait from 'simply-utils/dist/async/wait'
-import { CompanyType } from 'src/models/fundPriceRecord/FundPriceRecord.type'
+import { CompanyType, FundPriceTableDetails } from 'src/models/fundPriceRecord/FundPriceRecord.type'
 
 import getTableDetails from 'src/models/fundPriceRecord/io/getTableDetails'
 
 export const DEFAULT_DELAY = 1000
-export type Iteratee <T> = (company: CompanyType, i: number, arr: CompanyType[]) => Promise<T>
+export type Iteratee <T> = (
+  company: CompanyType,
+  i: number,
+  arr: CompanyType[],
+  tableDetails: FundPriceTableDetails,
+) => Promise<T>
 export type GetIteratee <T> = (input: T) => Iteratee<T>
 
 const pipeByCompany = async <T = unknown> (
@@ -14,11 +19,12 @@ const pipeByCompany = async <T = unknown> (
   delay: number = DEFAULT_DELAY
 ): Promise<T> => {
   // Get list of companies from table-level "details" record
-  const { companies } = await getTableDetails()
+  const tableDetails = await getTableDetails()
+  const { companies } = tableDetails
   // Manipulation for each company
   return pipeAsync<T>(...companies.map(
     (company, i, arr) => async (input: T = initialInput) => {
-      const output = await getIteratee(input)(company, i, arr)
+      const output = await getIteratee(input)(company, i, arr, tableDetails)
       if (delay > 0 && i < arr.length - 1) await wait(delay)
       return output
     }
