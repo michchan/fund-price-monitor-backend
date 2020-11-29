18092af56db8e5ac9106a20e44ac5870aa2a85d1
Michael Chan <michchandev@gmail.com>
2020-11-30 00:24:45 +0800

    Use @michchan/webpack-config-aws-cdk

--- webpack.config.js
+++ webpack.config.js
@@ -1,102 +1,31 @@
 const fs = require('fs')
-const path = require('path')
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
-
-const DEFAULT_ANALYZER_PORT = 8888
-const createBundleAnalyzerPlugin = (reportTitle, portOffset = 0) => new BundleAnalyzerPlugin({
-  // Avoid CI crash issue
-  analyzerMode: 'static',
-  openAnalyzer: false,
-  reportTitle,
-  reportFilename: 'bundler_report.html',
-  analyzerPort: DEFAULT_ANALYZER_PORT + portOffset,
-})
-
-const mapEntry = handlersPath => {
-  const dirs = fs.readdirSync(handlersPath)
-  // Get names of js files
-  const names = dirs.filter(dir => {
-    const isSubDir = !/(\.js|\.ts)$/.test(dir)
-    if (isSubDir) {
-      const subDir = fs.readdirSync(`${handlersPath}/${dir}`)
-      // See if there is an index file for this sub directory
-      return subDir.filter(n => /^index.js$/.test(n))
-    }
-    return /\.js$/.test(dir)
-  })
-  // Map entry object
-  return names.reduce((obj, name) => {
-    // Remove .js file extension
-    const key = name.replace(/\.js$/, '')
-    // Append 'index.js' if it is a directory
-    const filePath = /\.js$/.test(name) ? name : `${name}/index.js`
-
-    return {
-      ...obj,
-      [key]: `./${handlersPath}/${filePath}`,
-    }
-  }, {})
-}
-
-const createConfig = (handlersPath, name, index) => ({
-  mode: 'development',
-  target: 'node',
-  entry: mapEntry(handlersPath),
-  output: {
-    path: `${process.cwd()}/${
-      handlersPath
-      // Replace '/build' with '/bundles' pathname
-        .replace(/build/i, 'bundles')
-      // Remove "/services"
-        .replace(/\/services/i, '')
-    }`,
-    // Keep the bundle name same as the orignal function name
-    filename: '[name].js',
-    libraryTarget: 'umd',
-  },
-  node: {
-    // Make sure that __dirname works in node env
-    __dirname: true,
-  },
-  module: {
-    rules: [
-      /**
-       * Use file loader to move chromnium .br files into /bin
-       * @link https://github.com/alixaxel/chrome-aws-lambda/issues/80
-       *
-       * This is for correctly bundling the chromnium instance required by 'chrome-aws-lambda',
-       * from the helper 'launchPuppeteerBrowserSession' of 'simply-utils'.
-       */
-      {
-        test: /chrome\-aws\-lambda\/bin\/(.+)\.br$/,
-        use: [{ loader: 'file-loader', options: { name: '/node_modules/chrome-aws-lambda/bin/[name].[ext]' } }],
-      },
-    ],
-  },
-  resolve: {
-    alias: {
-      src: path.resolve(__dirname, 'build'),
-    },
-  },
-  externals: {
-    // * Do NOT bundle 'aws-sdk' since it is included in the AWS Lambda NodeJS runtime
-    'aws-sdk': 'aws-sdk',
-  },
-  plugins: [createBundleAnalyzerPlugin(name, index)],
-})
+const { createConfig, createBundleAnalyzerPlugin } = require('@michchan/webpack-config-aws-cdk')
 
 // Services directory path
 const SERVICES_PATH = 'build/services'
+const DEFAULT_ANALYZER_PORT = 8888
 
 // Create config for handlers folder of each service
 module.exports = fs.readdirSync(SERVICES_PATH)
-  .map((path, i) => createConfig(`${SERVICES_PATH}/${path}/handlers`, path, i))
-
-/** Example output */
-// Module.exports = [
-//     CreateConfig('build/services/cron/handlers'),
-//     CreateConfig('build/services/api/handlers'),
-// ];
-/** And they will be bundled into: */
-// /bundles/cron/handlers
-// /bundles/api/handlers
+  .map((path, i) => {
+    const defaultConfig = createConfig(`${SERVICES_PATH}/${path}/handlers`, __dirname)
+    return {
+      ...defaultConfig,
+      plugins: [createBundleAnalyzerPlugin(path, DEFAULT_ANALYZER_PORT + i)],
+      module: {
+        rules: [
+          /**
+           * Use file loader to move chromnium .br files into /bin~
+           * @link https://github.com/alixaxel/chrome-aws-lambda/issues/80
+           *
+           * This is for correctly bundling the chromnium instance required by 'chrome-aws-lambda',
+           * from the helper 'launchPuppeteerBrowserSession' of 'simply-utils'.
+           */
+          {
+            test: /chrome\-aws\-lambda\/bin\/(.+)\.br$/,
+            use: [{ loader: 'file-loader', options: { name: '/node_modules/chrome-aws-lambda/bin/[name].[ext]' } }],
+          },
+        ],
+      },
+    }
+  })
