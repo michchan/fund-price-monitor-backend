bcf9790b22c3ea55c1693dc54ce22d93f45f3c5b
Michael Chan <michchandev@gmail.com>
2020-12-01 18:52:43 +0800

    Use composeEnv from simply-utils

--- construct/lib/env.ts
+++ construct/lib/env.ts
@@ -1,36 +1,11 @@
-import pick from 'lodash/pick'
+import composeEnv, { EnvValues as EV } from 'simply-utils/dist/devOps/composeEnv'
 
-// Config dot env
-require('dotenv').config()
-
-export interface EnvValues {
-  TELEGRAM_BOT_API_KEY_PARAMETER_NAME: string;
-  TELEGRAM_BOT_API_KEY_PARAMETER_VERSION: number;
-  TELEGRAM_CHAT_ID_PARAMETER_NAME: string;
-  GENERAL_LOG_SUBSCRIPTION_EMAIL: string;
-  AWS_DEFAULT_REGION: string;
-}
-
-const keys: (keyof EnvValues)[] = [
+const keys = [
   'TELEGRAM_BOT_API_KEY_PARAMETER_NAME',
   'TELEGRAM_BOT_API_KEY_PARAMETER_VERSION',
   'TELEGRAM_CHAT_ID_PARAMETER_NAME',
   'GENERAL_LOG_SUBSCRIPTION_EMAIL',
   'AWS_DEFAULT_REGION',
-]
-const values: EnvValues = pick(process.env, keys) as unknown as EnvValues
-
-/**
- * Throw an error if any of the environment variables is not defined
- */
-(() => {
-  for (const key of keys) {
-    const value = values[key]
-    if (value === undefined) throw new Error(`${key} is undefined but required in environment`)
-  }
-})()
-
-export default {
-  keys,
-  values,
-} as const
+] as const
+export interface EnvValues extends EV<typeof keys[number]> {}
+export default composeEnv(keys)
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,28 +1,7 @@
-import pick from 'lodash/pick'
+import composeEnv, { EnvValues as EV } from 'simply-utils/dist/devOps/composeEnv'
 
-// Config dot env
-require('dotenv').config()
-
-export interface EnvValues {
-  AWS_RUNTIME_REGION: string;
-}
-
-const keys: (keyof EnvValues)[] = [
+const keys = [
   'AWS_RUNTIME_REGION',
-]
-const values: EnvValues = pick(process.env, keys) as unknown as EnvValues
-
-/**
- * Throw an error if any of the environment variables is not defined
- */
-(() => {
-  for (const key of keys) {
-    const value = values[key]
-    if (value === undefined) throw new Error(`${key} is undefined but required in environment`)
-  }
-})()
-
-export default {
-  keys,
-  values,
-} as const
+] as const
+export interface EnvValues extends EV<typeof keys[number]> {}
+export default composeEnv(keys)
