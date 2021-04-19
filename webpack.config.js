c535c5b96da637f3b3aadb50a5210f7de0d5c2f1
Michael L.S. Chan <michael.ls.chan@axa.com.hk>
2021-04-19 15:44:39 +0800

    fix: Update tsconfig to include construct

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,17 @@ const fs = require('fs')
 const { createConfig, createBundleAnalyzerPlugin, chromeAWSLambdaRules } = require('@michchan/webpack-config-aws-cdk')
 
 // Services directory path
-const SERVICES_PATH = 'build/services'
+const SERVICES_PATH_INPUT = 'build/src/services'
+const SERVICES_PATH_OUTPUT = 'bundles'
+const SRC_ALIAS_OUTPUT = 'build/src'
 const DEFAULT_ANALYZER_PORT = 8888
 
 // Create config for handlers folder of each service
-module.exports = fs.readdirSync(SERVICES_PATH)
+module.exports = fs.readdirSync(SERVICES_PATH_INPUT)
   .map((path, i) => {
-    const defaultConfig = createConfig(`${SERVICES_PATH}/${path}/handlers`, __dirname)
+    const inputPath = `${SERVICES_PATH_INPUT}/${path}/handlers`
+    const outputPath = `${SERVICES_PATH_OUTPUT}/${path}/handlers`
+    const defaultConfig = createConfig(inputPath, outputPath, __dirname, SRC_ALIAS_OUTPUT)
     return {
       ...defaultConfig,
       plugins: [createBundleAnalyzerPlugin(path, DEFAULT_ANALYZER_PORT + i)],
