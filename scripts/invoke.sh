75f49c8d692373e842d3d3ec65ed1cfbc792703a
Michael Chan <michchandev@gmail.com>
2023-04-10 12:50:33 +0800

    dev: add script to clean log file

--- scripts/invoke.sh
+++ scripts/invoke.sh
@@ -16,5 +16,8 @@ echo "Concatenated params: $params"
 # Load .env
 set -o allexport; source .env; set +o allexport
 
+# Clean log file
+> .sam-local-dev-log
+
 # Invoke SAM CLI
 sam local invoke --profile=$AWS_PROFILE --region=$AWS_RUNTIME_REGION -l .sam-local-dev-log $1 $params
--- scripts/start-api.sh
+++ scripts/start-api.sh
@@ -3,5 +3,8 @@
 # Load .env
 set -o allexport; source .env; set +o allexport
 
+# Clean log file
+> .sam-local-dev-log
+
 # Start API CLI
 sam local start-api --profile=$AWS_PROFILE --port=$SAM_API_PORT --region=$AWS_RUNTIME_REGION -l .sam-local-dev-log
